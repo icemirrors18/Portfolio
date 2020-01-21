de5055399a31ed26de15dd77b0ccd9f76c545b68
Dalton Harenza <dalton.harenza@gmail.com>
2020-01-21 08:22:51 -0500

    Removed bad link in sidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -15,7 +15,6 @@ export default class Sidebar extends Component {
             <nav id="colorlib-main-menu" role="navigation" className="navbar">
               <div id="navbar" className="collapse">
                 <ul>
-                  <li className="active"><a href="#home" data-nav-section="home">Introduction</a></li>
                   <li><a href="#about" data-nav-section="about">About</a></li>
 				  <li><a href="#timeline" data-nav-section="timeline">Timeline</a></li>
 				  <li><a href="resume" data-nav-section="resume">Resume</a></li>
