c9450aa93621ad0d4e980c1760ebf739db6e9755
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-25 00:54:28 +0000

    Deploy icemirrors18/Portfolio to github.com/icemirrors18/Portfolio.git:gh-pages

---
+++ precache-manifest.fbfe00b4869884c156ed0c7cd21a2dec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f8c76b1e3550f961df7",
+    "url": "/Portfolio/static/css/main.c2e39988.chunk.css"
+  },
+  {
+    "revision": "9f8c76b1e3550f961df7",
+    "url": "/Portfolio/static/js/main.9f8c76b1.chunk.js"
+  },
+  {
+    "revision": "591eb5beb99e69ed8d3e",
+    "url": "/Portfolio/static/js/runtime~main.591eb5be.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/Portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "d3b0fb15471480ed4f43037641c66922",
+    "url": "/Portfolio/index.html"
+  }
+];
